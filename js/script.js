1b823a5b8767103a752662fbe9d40c85b93f6720
ishaaq08 <ishaaq001@gmail.com>
2023-09-11 12:06:42 +0100

    Ready to use APIs

--- js/script.js
+++ js/script.js
@@ -1,4 +1,21 @@
 const fruitForm = document.querySelector("#inputSection form")
-console.log(fruitForm);
 
-console.log("JS is working");
+const fruitList = document.querySelector("#fruitSection ul")
+
+const addFruit = fruit => {
+    const li = document.createElement("li")
+    li.textContent = fruit
+    fruitList.appendChild(li)
+
+    li.addEventListener("click", () => {
+        fruitList.removeChild(li)
+    })
+} 
+
+fruitForm.addEventListener("submit", e => {
+    e.preventDefault()
+    addFruit(e.target.fruitInput.value)
+    e.target.fruitInput.value = ""
+})
+
+
